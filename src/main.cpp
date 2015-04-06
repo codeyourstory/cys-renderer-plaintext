4edf0035cc33a3304941d15eee04af72475c2ece
Pierre-Emmanuel Lallemant <pe.lallemant@hotmail.fr>
2015-04-06 15:18:14 +0200

    updated .travis.yml

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cys-engine/cys-engine.h>
+
+using namespace std;
+
+int main(int argc, const char *argv[])
+{
+  cout << "cys-renderer-plaintext" << endl;
+  
+  return 0;
+}
+
